31c150fd5af2f6ef4680506a1d0d028335deca22
markoshura <markoshura@yandex.ru>
2015-10-27 01:22:01 +0300

    Update solar_model.py

--- solar_model.py
+++ solar_model.py
@@ -7,43 +7,37 @@ gravitational_constant = 6.67408E-11
 
 def calculate_force(body, space_objects):
     """Вычисляет силу, действующую на тело.
-
     Параметры:
-
     **body** — тело, для которого нужно вычислить дейстующую силу.
     **space_objects** — список объектов, которые воздействуют на тело.
     """
 
     body.Fx = body.Fy = 0
     for obj in space_objects:
-        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        if  r != 0:
-            body.Fx += (gravitational_constant * body.m * obj.m * abs(body.x - obj.x))/(r ** 3)
-            body.Fy += (gravitational_constant * body.m * obj.m * abs(body.y - obj.y))/(r ** 3)
+        if body == obj:
+            continue
+        r = ((body.x -obj.x)**2 + (body.y - obj.y)**2)**0.5
+        body.Fx += ((gravitational_constant*body.m*obj.m)/(r**2))*((obj.x - body.x)/r)
+        body.Fy += ((gravitational_constant*body.m*obj.m)/(r**2))*((obj.y - body.y)/r)
 
 
 def move_space_object(body, dt):
     """Перемещает тело в соответствии с действующей на него силой.
-
     Параметры:
-
     **body** — тело, которое нужно переместить.
     """
 
     ax = body.Fx/body.m
-    body.x += body.Vx * dt + 0.5 * ax * (dt ** 2)
-    body.Vx += ax * dt
-
+    body.x += body.Vx*dt +(ax*(dt)**2)/2
+    body.Vx += ax*dt
     ay = body.Fy/body.m
-    body.y += body.Vy * dt + 0.5 * ay * (dt ** 2)
-    body.Vy += ay * dt
+    body.y += body.Vy*dt +(ay*(dt)**2)/2
+    body.Vy += ay*dt
 
 
 def recalculate_space_objects_positions(space_objects, dt):
     """Пересчитывает координаты объектов.
-
     Параметры:
-
     **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
     **dt** — шаг по времени
     """
