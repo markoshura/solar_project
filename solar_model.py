0f21378cc276a9ad8674bc093a3fedfb9ec213fc
markoshura <markoshura@yandex.ru>
2015-12-01 22:09:28 +0300

    Update solar_model.py

--- solar_model.py
+++ solar_model.py
@@ -2,23 +2,31 @@
 # license: GPLv3
 
 gravitational_constant = 6.67408E-11
+G=gravitational_constant
 """Гравитационная постоянная Ньютона G"""
 
 
-def calculate_force(body, space_objects):
+def calculate_force(body, space_objects, physical_time):
     """Вычисляет силу, действующую на тело.
     Параметры:
     **body** — тело, для которого нужно вычислить дейстующую силу.
     **space_objects** — список объектов, которые воздействуют на тело.
     """
-
+    maxF=0
+    attr=None
     body.Fx = body.Fy = 0
     for obj in space_objects:
-        if body == obj:
-            continue
-        r = ((body.x -obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += ((gravitational_constant*body.m*obj.m)/(r**2))*((obj.x - body.x)/r)
-        body.Fy += ((gravitational_constant*body.m*obj.m)/(r**2))*((obj.y - body.y)/r)
+        if body is obj:
+            continue  # тело не действует гравитационной силой на само себя!
+        dx, dy = obj.x - body.x, obj.y - body.y
+        r_sq = dx**2 + dy**2
+        r = r_sq**0.5
+        force = G * body.m * obj.m / r_sq
+        body.Fx += force * dx / r  # FIXME?: нужно вывести формулу...
+        body.Fy += force * dy / r  # FIXME?: нужно вывести формулу...
+        if force>maxF: attr, maxF = obj, force
+        body.attractor = attr
+        body.time = physical_time
 
 
 def move_space_object(body, dt):
@@ -28,14 +36,15 @@ def move_space_object(body, dt):
     """
 
     ax = body.Fx/body.m
-    body.x += body.Vx*dt +(ax*(dt)**2)/2
+    body.x += body.Vx*dt + ax*dt*dt/2  # FIXME?: не понимаю как менять...
     body.Vx += ax*dt
+    
     ay = body.Fy/body.m
-    body.y += body.Vy*dt +(ay*(dt)**2)/2
+    body.y += body.Vy*dt + ay*dt*dt/2 
     body.Vy += ay*dt
 
 
-def recalculate_space_objects_positions(space_objects, dt):
+def recalculate_space_objects_positions(space_objects, dt, t):
     """Пересчитывает координаты объектов.
     Параметры:
     **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
@@ -43,7 +52,7 @@ def recalculate_space_objects_positions(space_objects, dt):
     """
 
     for body in space_objects:
-        calculate_force(body, space_objects)
+        calculate_force(body, space_objects, t)
     for body in space_objects:
         move_space_object(body, dt)
 
